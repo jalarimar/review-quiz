c90be04f0bf4a4c148888e37b69e89144962ed1c
j <jalarimar@gmail.com>
2024-11-26 17:35:03 +0000

    Use materialUI, shuffle answer choice list, change colour depending on where selected all right or any wrong

--- review-quiz/app/page.tsx
+++ review-quiz/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
 import Link from 'next/link';
-import {NextUIProvider} from "@nextui-org/system";
 
 export default function Home() {
   return (
-    <NextUIProvider>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -104,6 +102,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-    </NextUIProvider>
   );
 }
--- review-quiz/app/quiz/page.tsx
+++ review-quiz/app/quiz/page.tsx
@@ -1,19 +1,83 @@
+'use client'
+import { FormControlLabel, FormGroup, Checkbox, Button } from "@mui/material";
 import Q from "./questions.json";
-import {Checkbox} from "@nextui-org/react";
+import React from "react";
+import dynamic from 'next/dynamic';
+import { green, red, grey } from '@mui/material/colors';
 
 
-export default function Home() {
+export default dynamic(() => Promise.resolve(Home), {
+  ssr: false
+})
+
+function Home() {
   var question1 = Q.questions[0];
+
+  const [isCorrect, setIsCorrect] = React.useState(2);
+  const [options] = React.useState(Shuffle(question1.answers, question1.herrings));
+  const [selected, setSelected] = React.useState<string[]>([]);
+
+  function CheckAnswer(ans: string, isSelected: boolean) {
+    var newSelected: string[] = [];
+    if (isSelected && !selected.includes(ans)) {
+      newSelected = selected.concat([ans]);
+    } else if (!isSelected) {
+      newSelected = selected;
+      const index = newSelected.indexOf(ans, 0);
+      if (index > -1) { // remove if contains ans
+        newSelected.splice(index, 1);
+      }
+    }
+    setSelected(newSelected);
+
+    // colour grey
+    setIsCorrect(2);
+    if (question1.answers.every((a) => newSelected.includes(a))) {
+      // colour green
+      setIsCorrect(1);
+    } 
+    if (newSelected.some((s) => question1.herrings.includes(s))) {
+      // colour red
+      setIsCorrect(0);
+    }
+  }
+  
   return (
-    <div>
-        {question1.question}
-        <ol>
-          {question1.answers.map((ans, i) =>
-            <li key={i}>
-              <Checkbox>{ans}</Checkbox>
-            </li>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <FormGroup>
+          {question1.question}
+          {options.map((ans, i) =>
+            <FormControlLabel 
+              key={i} 
+              control={<Checkbox onChange={(event) => CheckAnswer(ans, event.target.checked)} />} 
+              style={{
+                color: isCorrect > 1 ? grey[600] // default state
+                      : isCorrect > 0 ? green[600] 
+                      : red[600]}}
+              label={ans} 
+            />
           )}
-        </ol>
+          <Button variant="outlined">Next</Button>
+        </FormGroup>
+      </main>
     </div>
   );
+}
+
+function Shuffle(list1: string[], list2: string[]) {
+  // Fisher-Yates shuffle
+  var choices = list1.concat(list2);
+  var shuffled = [];
+  while (choices.length > 0) {
+    var k = getRandomIntUpTo(choices.length);
+    if (k == choices.length) continue;
+    shuffled.push(choices[k]);
+    choices.splice(k, 1); // remove k
+  }
+  return shuffled
+}
+
+function getRandomIntUpTo(max: number) {
+  return Math.floor(Math.random() * max);
 }
