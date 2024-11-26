b26c867a53d473c082b5bc4868fe156514dcbc76
j <jalarimar@gmail.com>
2024-11-26 11:53:24 +0000

    Add quiz page

--- review-quiz/app/page.tsx
+++ review-quiz/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -21,7 +22,10 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Snow is cold</li>
+          <Link key="quiz"
+            href="/quiz">
+              <li>Snow is cold</li>
+          </Link>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -38,7 +42,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deployed on Vercel
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
